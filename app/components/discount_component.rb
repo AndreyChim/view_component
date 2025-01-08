e6a89d92005c3557d6933b6b97363b9d5dcfcd29
AndreyChim <chimitcyrenov@mail.ru>
2025-01-08 19:53:50 +0800

    Generate component Discount

---
+++ app/components/discount_component.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class DiscountComponent < ViewComponent::Base
+  def initialize(item:)
+    @item = item
+  end
+
+  def render?
+    @item.price >= 100
+  end
+  
+end
